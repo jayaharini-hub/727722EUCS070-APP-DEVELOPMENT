5b21930d2ea31c2dd65ef64547d73c8d41d1253f
jayaharini-hub <125259320+jayaharini-hub@users.noreply.github.com>
2024-07-24 09:14:31 +0530

    Add files via upload

---
+++ day2(2)/src/App.js
@@ -0,0 +1,50 @@
+
+import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+
+
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 1);
+      }, 1000);
+    } else if (!isRunning && time !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isRunning, time]);
+
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setTime(0);
+  };
+
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <p>{new Date(time * 1000).toISOString().substr(11, 8)}</p>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+};
+
+
+
+
+export default App;
